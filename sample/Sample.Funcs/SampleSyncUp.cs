a70cd34a70662bb79f65d6f085901ce5f0a850ec
alex <kpobb1989@gmail.com>
2024-02-25 09:44:50 -0800

    Added cancellation token

--- sample/Sample.Funcs/SampleSyncUp.cs
+++ sample/Sample.Funcs/SampleSyncUp.cs
@@ -11,14 +11,14 @@ namespace Sample.Funcs
     {
 
         [Function("SampleSyncUp")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] CancellationToken ct)
         {
             var user = new UserEntity() { Email = "sportjoy@outlook.com", FirstName = "Vyasya", LastName = "Pupkin" };
             var user2 = new UserEntity() { Email = "f0rever@i.ua",  FirstName = "Alex", LastName = "Kushnir" };
 
-            await unitOfWork.Entity<UserEntity>().SyncUpAsync(new[] { user, user2 });
+            await unitOfWork.Entity<UserEntity>().SyncUpAsync(new[] { user, user2 }, ct: ct);
 
-            await unitOfWork.SaveChangesAsync();
+            await unitOfWork.SaveChangesAsync(ct);
 
             return new OkResult();
         }
