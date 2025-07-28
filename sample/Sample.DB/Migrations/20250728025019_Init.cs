f59ba8a79a767583ac9f5f2630c1010420ed9fed
kpobb1989 <kpobb1989@gmail.com>
2025-07-27 19:51:09 -0700

    Updated

---
+++ sample/Sample.DB/Migrations/20250728025019_Init.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sample.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorBooks",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorBooks", x => new { x.AuthorId, x.BookId });
+                    table.ForeignKey(
+                        name: "FK_AuthorBooks_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorBooks_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBooks_AuthorId_BookId",
+                table: "AuthorBooks",
+                columns: new[] { "AuthorId", "BookId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBooks_BookId",
+                table: "AuthorBooks",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_Email",
+                table: "Authors",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Title",
+                table: "Books",
+                column: "Title",
+                unique: true,
+                filter: "[Title] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorBooks");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- sample/Sample.DB/SampleDbContext.cs
+++ sample/Sample.DB/SampleDbContext.cs
@@ -14,10 +14,25 @@ namespace Sample.DB
         [DebuggerStepThrough]
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            // Unique indx for Title
+            builder.Entity<BookEntity>()
+                .HasIndex(s => s.Title)
+                .IsUnique();
+
+            // Unique indx for Email
+            builder.Entity<AuthorEntity>()
+                .HasIndex(s => s.Email)
+                .IsUnique();
+
             builder.Entity<AuthorBookEntity>()
                 .ToTable("AuthorBooks")
                 .HasKey(e => new { e.AuthorId, e.BookId });
             builder.Entity<AuthorBookEntity>().Ignore(e => e.Id);
+
+            // Unique index for AuthorId and BookId
+            builder.Entity<AuthorBookEntity>()
+                .HasIndex(e => new { e.AuthorId, e.BookId })
+                .IsUnique();
 
             builder.Entity<AuthorEntity>()
                 .HasMany(s => s.Books)
